4afbdad1be6c5814a407c84691cda23b0c0fbcc7
James Crockett <jecrockett@gmail.com>
2016-03-05 21:41:31 -0700

    Filter works to highlight specified user's sentiments

--- app/models/graph_manager.rb
+++ app/models/graph_manager.rb
@@ -1,16 +1,47 @@
 class GraphManager
+  attr_reader :channel, :user, :range
 
-  def set_graph_data(channel, user, range)
+  def initialize(channel, user, range)
+    @channel = channel
+    @user = user
+    @range = range
+  end
+
+  def set_chart_data
     if channel.nil? || channel.empty?
       nil
-    elsif user.empty?
-      sentiments_scoped(range).where(channel_id: channel).pluck(:slack_id, :score)
     else
-      sentiments_scoped(range).where(channel_id: channel, user_id: user).pluck(:slack_id, :score)
+      scoped_sentiments.where(channel_id: channel).pluck(:slack_id, :score)
     end
   end
 
-  def sentiments_scoped(range)
+  def set_channel_data
+    scoped_sentiments.where(channel_id: channel).where.not(user_id: user).pluck(:slack_id, :score)
+  end
+
+  def set_user_data
+    scoped_sentiments.where(channel_id: channel, user_id: user).pluck(:slack_id, :score)
+  end
+
+  def set_chart_title
+    if channel.nil? || channel.empty?
+      "Please select a channel from the dropdown menu."
+    elsif user.empty?
+      "#{range} Sentiments in #{channel_name}"
+    else
+      "#{range} Sentiments in #{channel_name} -- Highlighting: #{user_name}"
+    end
+  end
+
+  def channel_name
+    Channel.find(channel).name
+  end
+
+  def user_name
+    User.find(user).nickname
+  end
+
+  def scoped_sentiments
     return Sentiment.weekly  if range == "Weekly"
     return Sentiment.daily   if range == "Daily"
   end
