9c2aa487ff9da778fab5034739195b2dbd4dc385
James Crockett <jecrockett@gmail.com>
2016-03-07 18:38:24 -0700

    Create method for channels adds channel to database

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -8,7 +8,24 @@ class ChannelsController < ApplicationController
     @untracked_channels = untracked_channels
   end
 
+  def create
+    channel_info = parse_channel_params
+    channel = build_channel(channel_info)
+    if channel.save
+      flash[:notice] = "We'll start gathering data for #{channel.name}. Note: You will not see data for #{channel.name} appear right away."
+      redirect_to channels_path
+    else
+      flash[:error] = "Something went wrong. Try your selection again."
+      @untracked_channels = untracked_channels
+      render :new
+    end
+  end
+
   private
+
+    def parse_channel_params
+      params[:info].split.map { |info| info.gsub(/\W+/, '') }
+    end
 
     def untracked_channels
       slack = SlackService.new
@@ -19,12 +36,20 @@ class ChannelsController < ApplicationController
     def parse_untracked(channels)
       channels.map do |channel|
         next if already_tracking?(channel)
-        [channel['name'], channel['id']]
+        [channel['name'], [channel['name'], channel['id']]]
       end
     end
 
     def already_tracking?(ch)
       Channel.all.pluck(:slack_id).include?(ch['id'])
+    end
+
+    def build_channel(info)
+      c = Channel.new
+      c.name = info.first
+      c.slack_id = info.last
+      c.team_id = current_user.team_id
+      c
     end
 
 end
